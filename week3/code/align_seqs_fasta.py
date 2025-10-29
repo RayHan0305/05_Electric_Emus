467e13be719590243727875d3dda45bb17ec14e1
Natasha Lawson-Hale <tll25@ic.ac.uk>
2025-10-29 20:40:27 +0000

    Oct29th align_sequs_fasta: Finished- added doc strings and tested.

--- week3/code/align_seqs_fasta.py
+++ week3/code/align_seqs_fasta.py
@@ -1,7 +1,22 @@
 """
+align_seqs_fasta.py: Simple Pairwise Sequence Alignment
+
+This script performs an ungapped alignment search between two biological
+sequences provided in FASTA files. It determines the optimal alignment
+by sliding the shorter sequence across the entire length of the longer
+sequence and calculating the position with the highest number of direct matches.
 Usage:
-    python3 align_seqs_fasta ../data/<filename> ../data/<filename>
-"""
+    # To align two specific FASTA files:
+    python3 align_seqs_fasta.py <seq1_file_path> <seq2_file_path>
+    *NOTE*: these files should each only contain ONE sequence. 
+
+    # To use hard-coded default files:
+    python3 align_seqs_fasta.py """
+
+__appname__ = 'Align DNA squences and find best match'
+__author__ = 'Natasha Lawson-Hale (tll25@ac.ic.uk)'
+__version__ = '0.0.1'
+
 
 import sys
 
@@ -10,18 +25,42 @@ def read_fasta(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
         seq = "".join([line.strip() for line in lines if not line.startswith(">")])
-    return seq
 
+    return seq
 def calculate_score(s1, s2, l1, l2, startpoint):
-    # Calculate the score
+    """A function that computes a 'score' by returning the number of matches starting 
+    from the beggining of the longest sequence. This function sets the start point of 0 and
+    loops through every posisble alignment, it then saves the best alignment into 'match' and 
+    'score' showing at how many points in the sequence it aligns.
+
+    Doctests:
+    >>> calculate_score("ATGC", "ATGC", 4, 4, 0)
+    (4, '****')
+    >>> calculate_score("ATCC", "ATGC", 4, 4, 0)
+    (3, '**-*')
+    >>> calculate_score("TTGCAA", "ATGC", 6, 4, 0)
+    (3, '-***')
+    >>> calculate_score("AATGC", "ATTGCAG", 5, 7, 0)
+    (4, '*-***')
+    >>> calculate_score("GGG", "AAAAAA", 3, 6, 0)
+    (0, '---')
+    """
+    matched = "" # to hold string displaying alignements
     score = 0
     for i in range(l2):
-        if (i + startpoint) < l1:
-            if s1[i + startpoint] == s2[i]:
-                score += 1
-    return score
+        if (i + startpoint) < l1: #ensures the function doesnt run past the end of s1
+            if s1[i + startpoint] == s2[i]: # if the bases match
+                matched = matched + "*"
+                score = score + 1
+            else:
+                matched = matched + "-"
+    return score, matched
 
 def main(argv):
+    """
+    Handles command-line arguments, reads sequences, performs the sliding
+    alignment, and reports the single best alignment score and position.
+    """
     if len(argv) == 1:
      # Read the seq
         seq1 = read_fasta("../data/407228326.fasta")
@@ -31,13 +70,13 @@ def main(argv):
     elif len(argv) != 3:
         seq1 = argv[1]
         seq2 = argv[2]
-        print("Input found! Aligning: {seq1} and {seq2}")
+        print("Input found! Aligning.....")
 
     else:
         print ("Error: too few/many arguments or file error!")
         ("\nInput 3 arguments: align_seq_fasta.py and two fasta files: <seq1_file> <seq2_file>")
         print("\n OR Input ONLY: align_seq_fasta.py to use the default files.")
-    sys.exit(0)
+        sys.exit(0)
 
     # Compare
     l1, l2 = len(seq1), len(seq2)
